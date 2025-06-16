03ae367f46f6b214e1b91ede41aea4d09b41ef68
Ivan Carraco <83261520+I-CV207@users.noreply.github.com>
2025-06-16 10:14:01 -0600

    Delete page_aprende.py

--- page_aprende.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-
-st.title("Fintruth - Aprende")
-st.header("Aprende",divider=True)
-st.markdown('Consulta los siguientes recursos')
